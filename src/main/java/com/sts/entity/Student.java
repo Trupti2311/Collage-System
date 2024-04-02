362600117379f42df533fd1c9f033837b882982f
Trupti2311 <trane3629@gmail.com>
2024-04-02 11:47:12 +0530

    College system project created

---
+++ src/main/java/com/sts/entity/Student.java
@@ -0,0 +1,8 @@
+package com.sts.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Student {
+
+}
---
+++ src/main/java/com/sts/repository/StudentRepo.java
@@ -0,0 +1,8 @@
+package com.sts.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentRepo {
+
+}
