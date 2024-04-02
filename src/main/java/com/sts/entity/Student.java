33a62a6315da6dfa4f42213465b3b31898f983f1
Swapnil-197 <sspatil1907@gmail.com>
2024-04-02 21:40:14 +0530

    Pojo code updated

--- src/main/java/com/sts/entity/Student.java
+++ src/main/java/com/sts/entity/Student.java
@@ -1,8 +1,28 @@
 package com.sts.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 
 @Entity
 public class Student {
-
+ 
+	@Id
+	@Column(nullable = false , name = "first_name")
+	private String firstName ;
+	@Column(nullable = false , name = "last_name")
+	private String lastName ;
+	@Column(nullable = false , name = "date_of_birth")
+	private String dateOfBirth ;
+	@Column(nullable = false , name = "email")
+	private String email ;
+	@Column(nullable = false , name = "email")
+	private String phoneNumber ;
+	
+	@OneToOne
+	public Address address ;
+	
+	
+	
 }
